e56d9f87302baa72758c5438c772a8b54ba21745
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2023-05-07 16:53:39 +0300

    Update CircleApplication.java

--- Basic Concepts and Variables/CircleApplication.java
+++ Basic Concepts and Variables/CircleApplication.java
@@ -4,18 +4,18 @@ public class CircleApplication {
     public static void main(String[] args) {
         double radius,angle,area,perimeter,pi=3.14,circleSliceArea;
         Scanner inputRadius = new Scanner(System.in);
-        System.out.print("Dairenin yarıçapını giriniz: ");
+        System.out.print("Enter the radius of the circle: ");
         radius = inputRadius.nextDouble();
         area = pi * radius * radius;
         perimeter = 2 * pi * radius;
-        System.out.println("Dairenin çevresi: " + perimeter);
-        System.out.println("Dairenin alanı: " + area);
+        System.out.println("Perimeter of circle " + perimeter);
+        System.out.println("Area of circle " + area);
 
         Scanner inputAngle = new Scanner(System.in);
-        System.out.print("Daire diliminin açısını giriniz: ");
+        System.out.print("Enter the angle of the circle segment: ");
         angle = inputAngle.nextDouble();
-        circleSliceArea = (pi * (radius*radius) * angle)/360;
-        System.out.println("Daire diliminin alanı: " + circleSliceArea);
+        circleSliceArea = (pi * (radius * radius) * angle) / 360;
+        System.out.println("Area of the circle segment: " + circleSliceArea);
 
 
     }
